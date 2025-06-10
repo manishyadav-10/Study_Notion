649565e8455a00e937daaeaf2fe308c7c7e4993a
manishyadav-10 <my5170626@gmail.com>
2025-06-10 17:08:46 +0530

    Update database.js

--- Server/config/database.js
+++ Server/config/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 require("dotenv").config();
 
 exports.connect =()=>{
-    mongoose.connect(process.env.MONGODB_URL,{
+    mongoose.connect(process.env.DB_URI,{
         useNewUrlParser: true,
         useUnifiedTopology: true,
     })
