ca38ec47f963e79f58f9d763e56cc741d16462e8
manishyadav-10 <my5170626@gmail.com>
2025-06-12 13:14:20 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const userRoutes = require("./routes/User");
 const profileRoutes = require("./routes/Profile");
 const paymentRoutes = require("./routes/Payments");
 const courseRoutes = require("./routes/Course");
-
+const contactUsRoute = require("./routes/Contact");
 const database = require("./config/database");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
@@ -25,7 +25,7 @@ app.use(cookieParser());
 app.use(
     cors({
         // origin :"http://localhost:3000",
-         origin: "https://study-notion-i2w6.vercel.app", 
+         origin: "*", 
         credentials:true,
     })
 )
@@ -46,6 +46,7 @@ app.use("/api/v1/auth", userRoutes);
 app.use("/api/v1/profile", profileRoutes);
 app.use("/api/v1/course", courseRoutes);
 app.use("/api/v1/payment", paymentRoutes);
+app.use("/api/v1/reach",contactUsRoute);
 
 // deafault route 
 
